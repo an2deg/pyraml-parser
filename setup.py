9b7f838f705ed31563b0b8d3230a782ba9cb34d9
Andrii Degtiarov <andrew.degtiariov@gmail.com>
2014-06-17 18:08:17 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyraml-parser',
-    version='0.0.1',
+    version='0.0.2',
     author='Andrii Degtiarov',
     author_email='andrew.degtiariov@gmail.com',
     packages=['pyraml'],
