3753ec66ecc108db9b73805ce91bb49326940cb8
Andrii Degtiarov <andrew.degtiariov@gmail.com>
2014-12-11 19:56:42 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyraml-parser',
-    version='0.0.2',
+    version='0.0.5',
     author='Andrii Degtiarov',
     author_email='andrew.degtiariov@gmail.com',
     packages=['pyraml'],
