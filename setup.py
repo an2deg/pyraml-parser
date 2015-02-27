588e7ee7f2d70bac17976b4642152b279f8f0e29
Artem Kostiuk <postatum@gmail.com>
2015-02-27 18:12:24 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyraml-parser',
-    version='0.1.0',
+    version='0.1.1',
     author='Andrii Degtiarov',
     author_email='andrew.degtiariov@gmail.com',
     packages=['pyraml'],
