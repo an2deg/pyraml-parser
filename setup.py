1344c75ec48cbc91f79eb5dd9644dd9053fe12fc
Artem Kostiuk <postatum@gmail.com>
2015-04-28 18:35:32 +0300

    Add CHANGES.txt. Bump version and development status

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+import os
 from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.md')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
 
 setup(
     name='pyraml-parser',
-    version='0.1.1',
+    version='0.1.2',
     author='Andrii Degtiarov',
     author_email='andrew.degtiariov@gmail.com',
     packages=['pyraml'],
     url='https://github.com/an2deg/pyraml-parser',
     license='MIT',
     description='Python parser for RAML.',
-    long_description=open('README.md').read(),
+    long_description=README + '\n\n' + CHANGES,
     install_requires=[
         'setuptools',
         'PyYAML>=3.10',
@@ -19,7 +31,7 @@ setup(
     zip_safe=True,
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
